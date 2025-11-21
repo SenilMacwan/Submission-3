78a9651f650ad60818d38a9ea170d496efc97dfc
SenilMacwan <f5rx8@unb.ca>
2025-11-21 13:07:07 -0400

    Add files via upload

--- COORD.java
+++ COORD.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class COORD here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author senil macwan
+ * @version version 1.3
  */
 public class COORD extends ListTC
 {
@@ -12,11 +12,18 @@ public class COORD extends ListTC
         TestCase myTC = new TestCase(title, input, Exoutput);
         add(myTC);
     }
-
+    
     public void LoadTestCase(String fileName)
     {
         TestCase myTC = new TestCase("",0,0);
         myTC.initFromFile(fileName);
         add(myTC);
     }
+    
+    public void SAVETESTCASE(String Title, String Filename)
+    {
+        TestCase T = search(Title);
+        T.save(Filename);
+    }            
+        
 }
