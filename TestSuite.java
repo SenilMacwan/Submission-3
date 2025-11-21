3919800ea8a234c9874807a0779663dbbda40829
Abdullah Tauqir <abdullahtauqir._.735@unb.ca>
2025-11-20 22:32:08 -0400

    Update TestSuite.java

--- TestSuite.java
+++ TestSuite.java
@@ -1,1 +1,28 @@
+/**************************************************
+* This program demonstrate test suite by following
+* test case.
+*
+*@author Abdullah Tauqir
+*@Project CS-2043/Group 10
+***************************************************/ 
+
+import java.util.ArrayList;
+
+public class TestSuite 
+{
+
+    public String name;
+    public ArrayList<TestCase> listTC;
+
+    public TestSuite(String name) 
+  {
+        this.name = name;
+        this.listTC = new ArrayList<>();
+  }
+
+    public void addTestCase(TestCase tc)
+  {
+        listTC.add(tc);
+  }
+}
 
