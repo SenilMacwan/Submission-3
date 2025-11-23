d1be6f31d2c3405128bdb1c61a9eb3ad21a73144
Sanjida-49 <SanjidaShakhayet@gmail.com>
2025-11-22 22:17:41 -0600

    Update ListOfTestSuite.java

--- ListOfTestSuite.java
+++ ListOfTestSuite.java
@@ -4,26 +4,27 @@ public class ListOfTestSuites
 {
 
     private ArrayList<TestSuite> suites;
-
+   // Constructor
     public ListOfTestSuites() 
   {
         suites = new ArrayList<>();
     }
-
+    //Adds a TestSuite to the collection
     public void add(TestSuite ts) 
   {
         suites.add(ts);
     }
-
+  // Searches the list for a TestSuite with the given name
     public TestSuite search(String name) 
   {
         for (TestSuite t : suites) 
-        {
+        {   // Compare suite names
             if (t.name.equals(name)) 
-            {
+            {   // Return the matching TestSuite
                 return t;
             }
         }
+       // If no match found, return null
         return null;
     }
 }
