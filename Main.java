59dfc1672bb70073f491db1e5b4e3419ff0170fa
Abdullah Tauqir <abdullahtauqir._.735@unb.ca>
2025-11-23 16:04:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-/*****************************************************
-* This program demonstrate the main class to run UI
-*@author Abdullah Tauqir
+/******************************************************************
+* Entry point of the application, initializes and runs the program.
+*
 * CS2043 / Group10
-****************************************************/
+*******************************************************************/
 public class Main
     {
     public static void main(String[] args)
