40fdcedc80dd01558021bcbed1b33d435eb0b1d6
Ayman-UNB <amalik4@unb.ca>
2025-11-24 09:49:43 -0400

    Update TestSuite.java

--- TestSuite.java
+++ TestSuite.java
@@ -9,16 +9,19 @@ import java.util.ArrayList;
 
 public class TestSuite 
 {
- // Instance Variables
+    // Instance Variables
     public String name;
     public ArrayList<TestCase> listTC;
+ 
     // Constructor
     public TestSuite(String name) 
-    {   // Assign the suite name
+    {   
+        // Assign the suite name
         this.name = name;
         // Create an empty list of TestCases
         this.listTC = new ArrayList<>();
     }
+ 
     // method for adding testcases 
     //This method is called whenever the user creates or loads
     public void addTestCase(TestCase tc)
