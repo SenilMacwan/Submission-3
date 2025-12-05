9dfe93251f1732498f749ad2e54cfbfa9cb60921
Abdullah Tauqir <abdullahtauqir._.735@unb.ca>
2025-12-05 12:26:19 -0400

    Update TestSuite.java
    adding helper method to get total cases and number of passed test cases. finally count how many times we have done.

--- TestSuite.java
+++ TestSuite.java
@@ -25,4 +25,17 @@ public class TestSuite
         listTC.add(tc);
     }
 
+    public int getTotalTestCases() {
+    return testCases.getCount();
+}
+
+public int getPassedTestCases() {
+    int count = 0;
+    for (int i = 0; i < testCases.getCount(); i++) {
+        if (testCases.getAt(i).isPassed()) {
+            count++;
+        }
+    }
+    return count;
+}
 }
