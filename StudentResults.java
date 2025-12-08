2b05cad22ce1f7b4cdd19de5f18c1416dc01924c
Ayman-UNB <amalik4@unb.ca>
2025-12-07 21:36:21 -0400

    Create StudentResults.java
    Student Results class for storing the test results

---
+++ StudentResults.java
@@ -0,0 +1,46 @@
+import java.io.Serializable;
+import java.util.Map;
+/**
+ * A class that will help store student results.
+ *
+ * @author CS 2043 Group 10
+ * @version (6th Nov 2025)
+ */
+
+public class StudentResults implements Serializable
+{
+    // Name of student submitting the folder
+    private String studentName;
+    
+    // Path to the coded folder
+    private String folderPath;
+    
+    // Number of test cases in the test suite and how many passed
+    private int totalTests;
+    private int passedTests;
+    
+    private Map<String, Boolean> testResults;
+    
+    // Constructor Method
+    public StudentResults( String studentName, String folderPath, int totalTests, int passedTests, Map <String,Boolean> testResults)
+    {
+        this.studentName = studentName;
+        this.folderPath = folderPath;
+        this.totalTests = totalTests;
+        this.passedTests = passedTests;
+        this.testResults = testResults;
+    }
+    
+    // Accessor Methods
+    public String getStudentName() { return studentName; }
+
+    public String getFolderPath() { return folderPath; }
+
+    public int getTotalTests() { return totalTests; }
+
+    public int getPassedTests() { return passedTests; }
+
+    public Map<String, Boolean> getTestResults() { return testResults; }
+    
+}
+
